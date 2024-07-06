ae5721cf4ca876d9e31678602d19c373013c92e0
Numan Khalil <numankhalil27@gmail.com>
2024-07-06 12:55:51 +0500

    Home & About Content Updated

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,6 +4,7 @@ const About = () => {
   return (
     <div className="min-vh-100 d-flex align-items-center justify-content-center">
       <h1>About Page Content</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora corporis fugit ut temporibus asperiores? Voluptates sapiente debitis tempore dicta vero magni velit ipsa voluptate. Praesentium dolor perspiciatis fugiat deleniti debitis?</p>
     </div>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,8 @@ const Home = () => {
     <div className="min-vh-100 d-flex align-items-center justify-content-center">
       {/* <Header/> */}
       <h1>Home Page Content</h1>
+
+      <h3>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem, eum.</h3>
     </div>
   );
 };
